0e63edba1e944d8c53bf2613f9fdc43d0ce56673
Bruce Hill <bhill@slac.stanford.edu>
2021-11-04 03:05:29 -0700

    Update pgpClSerialTool for up900 serial testing.

--- pgpCamlinkApp/src/pgpClSerialTool.cpp
+++ pgpCamlinkApp/src/pgpClSerialTool.cpp
@@ -41,7 +41,7 @@ std::string	MapEscapeChars( const std::string & in )
 long
 SendMsgLoop(
 	pgpClSerialDev	&	pgpDev,
-  std::string&  detType,
+	std::string		&	detType,
 	double				timeout,
 	int					verbose	)
 {
@@ -51,113 +51,119 @@ SendMsgLoop(
 	std::string		sendBuffer;
 
 	std::cout << "Hit Ctrl-C to exit ...\r" << std::endl;
-  if (detType == "opal")
-  {
-    std::cout << "Sending test msg: @SN?\r" << std::endl;
-    pgpDev.sendString( "@SN?\r" );	// Test Pattern On
-  }
-  else if (detType == "basler")
-  {
-    uint8_t* frame = reinterpret_cast<uint8_t*>(inBuffer);
-    size_t   frameSize = sizeof(inBuffer);
-    std::string msg("C32 0x0108?");
-    std::cout << "Sending test msg: '" << msg << "'" << std::endl;
-    bbp::verbose = verbose;
-    auto rc = bbp::asciiToFrame(msg.c_str(), sizeof(inBuffer), frame, frameSize);
-    if (rc == bbp::FAILURE)
-    {
-      std::cerr << "Error creating input string for " << detType << std::endl;
-      return -1;
-    }
-    pgpDev.sendBytes( frame, frameSize );
-  }
-  else
-  {
-    std::cout << "Unrecognized device type " << detType << std::endl;
-    return -1;
-  }
+	if (detType == "opal")
+	{
+		std::cout << "Sending test msg: @SN?\r" << std::endl;
+		pgpDev.sendString( "@SN?\r" );	// Test Pattern On
+	}
+	else if (detType == "up900")
+	{
+		std::cout << "Sending test msg: rp\r" << std::endl;
+		pgpDev.sendString( "rp\r" );	// Report
+	}
+	else if (detType == "basler")
+	{
+		uint8_t* frame = reinterpret_cast<uint8_t*>(inBuffer);
+		size_t   frameSize = sizeof(inBuffer);
+		std::string msg("C32 0x0108?");
+		std::cout << "Sending test msg: '" << msg << "'" << std::endl;
+		bbp::verbose = verbose;
+		auto rc = bbp::asciiToFrame(msg.c_str(), sizeof(inBuffer), frame, frameSize);
+		if (rc == bbp::FAILURE)
+		{
+			std::cerr << "Error creating input string for " << detType << std::endl;
+			return -1;
+		}
+		pgpDev.sendBytes( frame, frameSize );
+	}
+	else
+	{
+		std::cout << "Unrecognized device type " << detType << std::endl;
+		return -1;
+	}
 
-  while( 1 )
+	while( 1 )
 	{
 		//sleep(1);
 		int nRead = pgpDev.readBytes( recvBuffer, timeout, S_SENDBUFFER_MAX );
 		if ( nRead <= 0 )
 			std::cerr << "No response!" << std::endl;
-    else
+		else
 		{
 			std::cout << "Rcvd: " << std::endl;
-      if (detType == "opal")
-      {
-        unsigned char	cData;
-        for ( size_t i=0; i < (size_t) nRead; ++i )
-        {
-          cData = recvBuffer[i];
-          switch( cData )
-          {
-            default:
-              if ( isprint(cData ) )
-                putchar( cData );
-              else
-                printf( "<\?\?\?>(0x%02X)\n", cData );
-              break;
-            case 0x00:	printf( "<NUL>(0x%02X)\n", cData );	break;
-            case 0x02:	printf( "<STX>(0x%02X)\n", cData );	break;
-            case 0x03:	printf( "<ETX>(0x%02X)\n", cData );	break;
-            case 0x04:	printf( "<EOT>(0x%02X)\n", cData );	break;
-            case 0x06:	printf( "<ACK>(0x%02X)\n", cData );	break;
-            case 0x08:	printf( "<BS>(0x%02X)\n",  cData );	break;
-            case 0x09:	printf( "<HT>(0x%02X)\n",  cData );	break;
-            case 0x15:	printf( "<NAK>(0x%02X)\n", cData );	break;
-            case 0x1B:	printf( "<ESC>(0x%02X)\n", cData );	break;
-            case '\n':	printf( "<NL>(0x%02X)\n",  cData );	break;
-            case '\r':	printf( "<CR>(0x%02X)\n",  cData );	break;
-            case 0x7F:	printf( "<DEL>(0x%02X)\n", cData );	break;
-          }
-        }
-      }
-      else if (detType == "basler")
-      {
-        char   outBuffer[bbp::MAX_DATA_SIZE];
-        size_t outSize = sizeof(outBuffer);
-        auto rc = bbp::frameToAscii(recvBuffer, nRead, outBuffer, outSize);
-        if (rc == bbp::FAILURE)
-        {
-          std::cerr << "Error analyzing output string for " << detType << std::endl;
-          return -1;
-        }
-        if (outSize > 0)
-          printf("Parsed %zu bytes:\n  '%s'\n", outSize, outBuffer);
-      }
+			if ( detType == "opal" || detType == "up900" )
+			{
+				unsigned char	cData;
+				for ( size_t i=0; i < (size_t) nRead; ++i )
+				{
+					cData = recvBuffer[i];
+					switch( cData )
+					{
+					default:
+						if ( isprint(cData ) )
+							printf( "%c    (0x%02X)\n", cData, cData );
+						else
+							printf( "<\?\?\?>(0x%02X)\n", cData );
+						break;
+					case 0x00:	printf( "<NUL>(0x%02X)\n", cData );	break;
+					case 0x02:	printf( "<STX>(0x%02X)\n", cData );	break;
+					case 0x03:	printf( "<ETX>(0x%02X)\n", cData );	break;
+					case 0x04:	printf( "<EOT>(0x%02X)\n", cData );	break;
+					case 0x06:	printf( "<ACK>(0x%02X)\n", cData );	break;
+					case 0x08:	printf( "<BS> (0x%02X)\n",  cData );	break;
+					case 0x09:	printf( "<HT> (0x%02X)\n",  cData );	break;
+					case 0x15:	printf( "<NAK>(0x%02X)\n", cData );	break;
+					case 0x1B:	printf( "<ESC>(0x%02X)\n", cData );	break;
+					case '\n':	printf( "<NL> (0x%02X)\n",  cData );	break;
+					case '\r':	printf( "<CR> (0x%02X)\n",  cData );	break;
+					case 0x7F:	printf( "<DEL>(0x%02X)\n", cData );	break;
+					}
+				}
+			}
+			else if (detType == "basler")
+			{
+				char   outBuffer[bbp::MAX_DATA_SIZE];
+				size_t outSize = sizeof(outBuffer);
+				auto rc = bbp::frameToAscii(recvBuffer, nRead, outBuffer, outSize);
+				if (rc == bbp::FAILURE)
+				{
+					std::cerr << "Error analyzing output string for " << detType << std::endl;
+					return -1;
+				}
+				if (outSize > 0)
+					printf("Parsed %zu bytes:\n  '%s'\n", outSize, outBuffer);
+			}
 		}
 
 		std::cout << "Send? ";
 		std::cin.getline( &inBuffer[0], S_SENDBUFFER_MAX );
-    if ((inBuffer[0] == 0) || (inBuffer[0] == 0x04))  break;
+		if ((inBuffer[0] == 0) || (inBuffer[0] == 0x04))
+			break;
 
-    if (detType == "opal")
-    {
-      sendBuffer = MapEscapeChars( std::string(inBuffer) );
-      if ( sendBuffer.size() == 0 )
-        break;
-      pgpDev.sendString( sendBuffer );
-    }
-    else if (detType == "basler")
-    {
-      uint8_t frame[bbp::MAX_FRAME_SIZE];
-      size_t  frameSize = sizeof(frame);
-      auto rc = bbp::asciiToFrame(inBuffer, sizeof(inBuffer), frame, frameSize);
-      if (rc == bbp::FAILURE)
-      {
-        std::cerr << "Error creating input string for " << detType << std::endl;
-        continue;
-      }
-      pgpDev.sendBytes( frame, frameSize );
-    }
-  };
+		if ( detType == "opal" || detType == "up900" )
+		{
+			sendBuffer = MapEscapeChars( std::string(inBuffer) );
+			if ( sendBuffer.size() == 0 )
+				break;
+			pgpDev.sendString( sendBuffer );
+		}
+		else if (detType == "basler")
+		{
+			uint8_t frame[bbp::MAX_FRAME_SIZE];
+			size_t  frameSize = sizeof(frame);
+			auto rc = bbp::asciiToFrame(inBuffer, sizeof(inBuffer), frame, frameSize);
+			if (rc == bbp::FAILURE)
+			{
+				std::cerr << "Error creating input string for " << detType << std::endl;
+				continue;
+			}
+			pgpDev.sendBytes( frame, frameSize );
+		}
+	};
 
-  // close();
+	// close();
 
-  return (1);
+	return (1);
 }
 
 void usage( const char * msg )
@@ -169,8 +175,8 @@ void usage( const char * msg )
 	printf( "       -v    Verbose mode.\n" );
 	printf( "       -b N  board number [0].\n" );
 	printf( "       -c N  Channel number [0].\n" );
-  printf( "       -t F  Timeout [0.5].\n" );
-	printf( "       -D S  Detector type [\"opal\"].\n" );
+	printf( "       -t F  Timeout [0.5].\n" );
+	printf( "       -D S  Detector type [\"opal\", \"up900\", \"basler\"].\n" );
 }
 
 
@@ -185,7 +191,7 @@ main( int argc, char **argv )
 
 	char c;
 	std::string device;
-  std::string detType("opal");
+	std::string detType("opal");
 	while((c = getopt(argc, argv, "hvc:b:t:D:")) != EOF)
 	{
 		switch(c)
@@ -205,9 +211,9 @@ main( int argc, char **argv )
 		case 't':
 			timeout = atof(optarg);
 			break;
-    case 'D':
-      detType = optarg;
-      break;
+		case 'D':
+			detType = optarg;
+			break;
 		case 'h':
 		default:
 			usage(argv[0]);
@@ -218,8 +224,8 @@ main( int argc, char **argv )
 //	terminateMemoryOrder.store(false, std::memory_order_release);
 //	signal(SIGINT, int_handler);
 
-  if ((timeout == 0.0))
-    timeout = 0.5;
+	if ((timeout == 0.0))
+		timeout = 0.5;
 
 	printf("-- Rogue Version: %s\n", rogue::Version::current().c_str());
 
@@ -241,5 +247,5 @@ main( int argc, char **argv )
 	}
 	pgpDev.disconnect();
 
-  exit(status);
+	exit(status);
 }
